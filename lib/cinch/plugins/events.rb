8fa0705081f6ac3cb113dfeb38ab42ed91e1522f
Ayonix <adrianleva@gmail.com>
2013-05-04 13:30:12 +0200

    Events init

---
+++ lib/cinch/plugins/events.rb
@@ -0,0 +1,42 @@
+require 'dm-core'  
+require 'dm-timestamps'  
+require 'dm-validations'  
+require 'dm-migration' 
+
+module Cinch
+	module Plugins
+		class Events
+			include Cinch::Plugin
+			set :prefix, '!events '
+
+			def initialize(m)
+				DataMapper.setup :default, "sqlite://#{config["db"]}"
+				auto_upgrade!
+			end
+
+			match /add (.+),(.+),(.+)/, method: :add
+			def add(m, text, location, time)
+				e = Event.new what: text, where: location, when: time
+				if e.save
+					m.reply(e saved)
+				else
+					m.reply("Something went wrong")
+				end
+			end
+
+			match /list/, method: :list
+			def list(m)
+				m.reply(Event.all(:when.gte => DateTime.now))
+			end
+
+			class Event
+				include DataMapper::Resource
+				property :id, Serial
+				property :where, String, required: true
+				property :when, DateTime, required: true
+				property :what, String, required: true
+				property :who, String
+			end
+		end
+	end
+end
